50c0c2bb4824a11f5b40309776cb7c2ddc9d2e66
SiddharthPersistent <46192158+SiddharthPersistent@users.noreply.github.com>
2019-02-21 16:08:10 +0530

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Creating directory structure in Edge node
+
+
+basedir=`pwd`/orgconnect
+
+
+mkdir -p $(pwd)/orgconnect output activedirectory employeeinputdata employee email historicalconnect
