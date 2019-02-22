69dd7a6ee96f69c301981460bfda970e13e4dd1d
SiddharthPersistent <46192158+SiddharthPersistent@users.noreply.github.com>
2019-02-22 13:32:49 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 
 #Creating directory structure in Edge node
 echo "Basedir command"
-basedir=`pwd`/orgconnect
-
+#basedir=`pwd`/orgconnect
+pwd
 echo "$basedir"
 
 mkdir -p $(pwd)/orgconnect output activedirectory employeeinputdata employee email historicalconnect
